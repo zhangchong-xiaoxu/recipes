5ce5b3086fb27fbcd93fac5ed9332f847439fddf
zhangchong-xiaoxu <80248349+zhangchong-xiaoxu@users.noreply.github.com>
2025-06-05 17:32:25 +0800

    Update example.ts

--- src/controllers/example.ts
+++ src/controllers/example.ts
@@ -1,9 +1,18 @@
-import type {MojoContext} from '@mojojs/core';
+import { DuckDBConnection, DuckDBInstance } from '@duckdb/node-api';
+import type { MojoContext } from '@mojojs/core';
 
 export default class Controller {
   // Render template "example/welcome.html.tmpl" with message
-  async welcome (ctx: MojoContext): Promise<void> {
-    ctx.stash.msg = 'Welcome to the mojo.js real-time web framework!';
-    await ctx.render();
+  async recipes(ctx: MojoContext): Promise<void> {
+    console.log('Loading recipes...');
+    let recipes: any[] = [];
+    await ctx.app.models.database.connection(async (connection: DuckDBConnection) => {
+      recipes = await ctx.app.models.recipes.listRecipes(connection);
+    });
+    // Now recipes is set
+    console.log(`Loaded ${recipes.length} recipes`);
+    ctx.stash.recipes = recipes;
+    ctx.stash.title = 'Recipes List';
+    await ctx.render({ view: 'example/recipes' });
   }
 }
